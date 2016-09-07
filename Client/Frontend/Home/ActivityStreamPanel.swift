2b96b4e2b0c75daeb8512c74bcbd8fbcc800513f
Stephan Leroux <sleroux@mozilla.com>
2016-09-07 15:52:41 -0400

    Part 2: Updates UI to have Highlight title instead of Recent Activity

--- Client/Frontend/Home/ActivityStreamPanel.swift
+++ Client/Frontend/Home/ActivityStreamPanel.swift
@@ -90,27 +90,27 @@ extension ActivityStreamPanel {
 
     enum Section: Int {
         case TopSites
-        case History
+        case Highlights
 
         static let count = 2
 
         var title: String? {
             switch self {
-            case .History: return "Recent Activity"
+            case .Highlights: return NSLocalizedString("activitystream.highlights.label", value: "Highlights", comment: "Section title label for Highlights")
             case .TopSites: return nil
             }
         }
 
         var headerHeight: CGFloat {
             switch self {
-            case .History: return 40
+            case .Highlights: return 40
             case .TopSites: return 0
             }
         }
 
         func cellHeight(traits: UITraitCollection, width: CGFloat) -> CGFloat {
             switch self {
-            case .History: return UITableViewAutomaticDimension
+            case .Highlights: return UITableViewAutomaticDimension
             case .TopSites:
                 if traits.horizontalSizeClass == .Compact && traits.verticalSizeClass == .Regular {
                     return CGFloat(Int(width / ASPanelUX.TopSiteDoubleRowRatio)) + ASPanelUX.PageControlOffsetSize
@@ -122,9 +122,9 @@ extension ActivityStreamPanel {
 
         var headerView: UIView? {
             switch self {
-            case .History:
+            case .Highlights:
                 let view = ASHeaderView()
-                view.title = "Recent Activity"
+                view.title = title
                 return view
             case .TopSites:
                 return nil
@@ -134,7 +134,7 @@ extension ActivityStreamPanel {
         var cellIdentifier: String {
             switch self {
             case .TopSites: return "TopSiteCell"
-            case .History: return "HistoryCell"
+            case .Highlights: return "HistoryCell"
             }
         }
 
@@ -175,7 +175,7 @@ extension ActivityStreamPanel {
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         switch Section(indexPath.section) {
-        case .History:
+        case .Highlights:
             let site = self.history[indexPath.row]
             showSiteWithURLHandler(NSURL(string:site.url)!)
         case .TopSites:
@@ -196,7 +196,7 @@ extension ActivityStreamPanel {
         switch Section(section) {
             case .TopSites:
                 return topSitesManager.content.isEmpty ? 0 : 1
-            case .History:
+            case .Highlights:
                  return self.history.count
         }
     }
@@ -208,7 +208,7 @@ extension ActivityStreamPanel {
         switch Section(indexPath.section) {
         case .TopSites:
             return configureTopSitesCell(cell, forIndexPath: indexPath)
-        case .History:
+        case .Highlights:
             return configureHistoryItemCell(cell, forIndexPath: indexPath)
         }
     }
@@ -314,7 +314,7 @@ class ASHeaderView: UIView {
         return titleLabel
     }()
 
-    var title: String = "" {
+    var title: String? {
         willSet(newTitle) {
             titleLabel.text = newTitle
         }
